1067a2b74774f7671f6bf0e92bba482ae38dda1b
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2022-10-07 02:24:16 +0200

    functioning parts of mandatory

--- src/draw.c
+++ src/draw.c
@@ -6,33 +6,40 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 10:49:48 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/06 20:42:56 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/06 23:58:25 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 #include <stdlib.h>
 
-//Function defines the parameters for the Bresenham line algorithm.
-void	bresenham_define(t_bresenham *param, t_pointpair points)
+//Function puts a pixel as long as it is within the window/image.
+static void	put_pixel(int x, int y, mlx_image_t *image)
 {
-	param->dx = abs(points.x2 - points.x1);
-	param->dy = -1 * abs(points.y2 - points.y1);
-	if (points.x1 < points.x2)
+	if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT)
+		mlx_put_pixel(image, x, y, 0xFFFFFFFF);
+}
+
+//Function defines the parameters for the Bresenham line algorithm.
+static void	bresenham_define(t_bresenham *param, t_pointpair *points)
+{
+	param->dx = abs(points->x2 - points->x1);
+	param->dy = -1 * abs(points->y2 - points->y1);
+	if (points->x1 < points->x2)
 		param->sx = 1;
 	else
 		param->sx = -1;
-	if (points.y1 < points.y2)
+	if (points->y1 < points->y2)
 		param->sy = 1;
 	else
 		param->sy = -1;
 	param->err = param->dx + param->dy;
-	param->x0 = points.x1;
-	param->y0 = points.y1;
+	param->x0 = points->x1;
+	param->y0 = points->y1;
 }
 
 //Function draws a line between points using Bresenham's algorithm.
-void	bresenham_draw(t_pointpair points, mlx_image_t *image)
+void	bresenham_draw(t_pointpair *points, mlx_image_t *image)
 {
 	t_bresenham	*param;
 
@@ -40,8 +47,8 @@ void	bresenham_draw(t_pointpair points, mlx_image_t *image)
 	bresenham_define(param, points);
 	while (image)
 	{
-		mlx_put_pixel(image, param->x0, param->y0, 0xFFFFFFFF);
-		if (param->x0 == points.x2 && param->y0 == points.y2)
+		put_pixel(param->x0, param->y0, image);
+		if (param->x0 == points->x2 && param->y0 == points->y2)
 			break ;
 		param->err2 = 2 * param->err;
 		if (param->err2 >= param->dy)
--- src/test/draw_main.c
+++ src/test/draw_main.c
@@ -2,6 +2,7 @@
 #include "../../fdf.h"
 #include <limits.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*
 //Function that serves for hook actions for mlx.
@@ -21,25 +22,60 @@ void	hook(void *param)
 int	main(void)
 {
 	mlx_image_t	*image;
-	t_mlxdata	mlxdata;
-	t_pointpair	pp;
+	t_mlxdata	*mlxdata;
+	t_pointpair	*pp;
 
-	mlxdata.mlx = mlx_init(600,600, "Draw Test", true);
-	image = mlx_new_image(mlxdata.mlx, 600, 600);
+	pp = (t_pointpair *)malloc(sizeof(t_pointpair));
+	mlxdata = (t_mlxdata *)malloc(sizeof(t_mlxdata));
+	mlxdata->mlx = mlx_init(WIDTH, HEIGHT, "Draw Test", true);
+	mlxdata->image = mlx_new_image(mlxdata->mlx, WIDTH, HEIGHT);
 	if (!image)
 	{
-		mlx_terminate(mlxdata.mlx);
+		mlx_terminate(mlxdata->mlx);
 		return (0);
 	}
-	pp.x1 = 50;
-	pp.y1 = 50;
-	pp.x2 = 450;
-	pp.y2 = 450;
-	bresenham_draw(pp, image);
-	mlx_image_to_window(mlxdata.mlx, image, 0, 0);
-	mlx_loop_hook(mlxdata.mlx, &hook, mlxdata.mlx);
-	mlx_loop(mlxdata.mlx);
-	mlx_terminate(mlxdata.mlx);
+	pp->x1 = 50;
+	pp->y1 = 50;
+	pp->x2 = 450;
+	pp->y2 = 700;
+	bresenham_draw(pp, mlxdata->image);
+	mlx_image_to_window(mlxdata->mlx, mlxdata->image, 0, 0);
+	mlx_loop_hook(mlxdata->mlx, &hook, mlxdata->mlx);
+	mlx_loop(mlxdata->mlx);
+	mlx_terminate(mlxdata->mlx);
+	return (0);
+}
+//*/
+
+/* draw_grid Test
+// gcc draw_main.c ../draw.c ../../libft/libft.a ../../mlx42/libmlx42.a -lglfw
+// gcc draw_main.c ../draw.c ../../minilibx/libmlx_intel-mac.a -lXext -lX11
+int	main(void)
+{
+	mlx_image_t	*image;
+	t_mlxdata	*mlxdata;
+	t_mapdata	*mapdata;
+	int			ret;
+
+	mapdata = (t_mapdata *)malloc(sizeof(t_mapdata));
+	mlxdata = (t_mlxdata *)malloc(sizeof(t_mlxdata));
+	mlxdata->mlx = mlx_init(600,600, "Grid Draw Test", true);
+	mlxdata->image = mlx_new_image(mlxdata->mlx, 600, 600);
+	if (!image)
+	{
+		mlx_terminate(mlxdata->mlx);
+		return (0);
+	}
+	ret = map_data(mapdata, "../../test_maps/42.fdf");
+	if (ret == -1)
+		return (0);
+	draw_grid(mapdata, mlxdata);
+	mlx_image_to_window(mlxdata->mlx, mlxdata->image, 0, 0);
+	mlx_loop_hook(mlxdata->mlx, &hook, mlxdata->mlx);
+	mlx_loop(mlxdata->mlx);
+	mlx_terminate(mlxdata->mlx);
+	free(mapdata);
+	free(mlxdata);
 	return (0);
 }
 //*/
