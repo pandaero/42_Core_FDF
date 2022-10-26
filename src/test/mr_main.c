e978739ab11f39adfb1941752d7af2e98c2b63ce
pandalaf <pandalaf@student.42wolfsburg.de>
2022-10-26 13:54:44 +0200

    improved comment and map_data test

--- src/test/mr_main.c
+++ src/test/mr_main.c
@@ -1,8 +1,8 @@
-//Tests for the functions in map_reader.c
+// Tests for the functions in map_reader.c
 #include "../../fdf.h"
 
-/* map_depth test
-// run: gcc -Wall -Werror -Wextra mr_main.c ../ *.c ../../libft/libft.a
+/* Test for map_depth
+// run: cc -Wall -Werror -Wextra mr_main.c ../map_reader.c ../utils.c ../../libft/libft.a
 #include <stdio.h>
 
 int	main(void)
@@ -12,8 +12,8 @@ int	main(void)
 }
 //*/
 
-/* map_width test
-// run: gcc -Wall -Werror -Wextra mr_main.c ../ *.c ../../libft/libft.a
+/* Test for map_width
+// run: cc -Wall -Werror -Wextra mr_main.c ../map_reader.c ../utils.c ../../libft/libft.a
 #include <stdio.h>
 
 int	main(void)
@@ -23,26 +23,30 @@ int	main(void)
 }
 //*/
 
-/* map_data test
-// run: gcc -Wall -Werror -Wextra mr_main.c ../ *.c ../../libft/libft.a
+/* Test for map_data
+// compile: cc -g -Wall -Wextra mr_main.c ../map_reader.c ../utils.c ../../libft/libft.a
+// run: ./a.out ../../test_maps/42.fdf
 #include <stdio.h>
 #include <stdlib.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	t_mapdata	*data;
 	int			ret;
 	int			i, j;
 
 	data = (t_mapdata *)malloc(sizeof(t_mapdata));
-	ret = map_data(data, "../../test_maps/42.fdf");
+	ret = map_data(data, argv[1]);
+	printf("Input file: %s\n", argv[1]);
 	printf("Return Value: %d\n", ret);
-	printf("Array:\n");
+	if (ret == -1)
+		return (0);
+	printf("Array: (%d x %d)\n", data->width, data->depth);
 	i = 0;
-	while (i < map_depth("../../test_maps/42.fdf"))
+	while (i < data->depth)
 	{
 		j = 0;
-		while (j < map_width("../../test_maps/42.fdf"))
+		while (j < data->width)
 		{
 			if (data->height_data[i][j] >= 10)
 				printf("%d ", data->height_data[i][j]);
@@ -53,6 +57,7 @@ int	main(void)
 		printf("\n");
 		i++;
 	}
+	free_twodee(data->height_data, data->depth);
 	free(data);
 	return (0);
 }
