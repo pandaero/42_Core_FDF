02598dba4ff9ec526c2316f42e3d262d5d282cb3
pandalaf <pandalaf@student.42wolfsburg.de>
2022-10-27 19:18:25 +0200

    added parallel projection

--- src/bonus/projection_bonus.c
+++ src/bonus/projection_bonus.c
@@ -6,11 +6,24 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 21:26:33 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/27 17:28:41 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/27 19:11:43 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../fdf_bonus.h"
+#include <math.h>
+
+//Function applies a parallel projection. Achieved using 45 deg.
+void	parallel(int *xco, int *yco, int zco)
+{
+	int	xcp;
+	int	ycp;
+
+	xcp = *xco;
+	ycp = *yco;
+	*xco = xcp - (zco * cos(0.785398));
+	*yco = ycp - (zco * sin(0.785398));
+}
 
 //Function applies projection to a point pair, given map data. Horizontal pairs.
 void	projected_horiz_bonus(t_pointpair *pp, t_fulldata *full, int x, int y)
@@ -25,13 +38,11 @@ void	projected_horiz_bonus(t_pointpair *pp, t_fulldata *full, int x, int y)
 		isometric(&pp->x1, &pp->y1, z1);
 		isometric(&pp->x2, &pp->y2, z2);
 	}
-	/*
 	else
 	{
-		parallel();
-		parallel();
+		parallel(&pp->x1, &pp->y1, z1);
+		parallel(&pp->x2, &pp->y2, z2);
 	}
-	*/
 }
 
 //Function applies projection to a point pair, given map data. Horizontal pairs.
@@ -47,11 +58,9 @@ void	projected_vert_bonus(t_pointpair *pp, t_fulldata *full, int x, int y)
 		isometric(&pp->x1, &pp->y1, z1);
 		isometric(&pp->x2, &pp->y2, z2);
 	}
-	/*
 	else
 	{
-		parallel();
-		parallel();
+		parallel(&pp->x1, &pp->y1, z1);
+		parallel(&pp->x2, &pp->y2, z2);
 	}
-	*/
 }
