1943321f563cb76fb16a197593fb5fd125f02be3
pandalaf <pandalaf@student.42wolfsburg.de>
2022-10-27 17:13:29 +0200

    norminette

--- src/bonus/frames.c
+++ src/bonus/frames.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 17:18:22 by pandalaf          #+#    #+#             */
-/*   Updated: 2022/10/27 17:08:56 by pandalaf         ###   ########.fr       */
+/*   Updated: 2022/10/27 17:12:42 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 //Function sets a window-sized image to black.
 void	wipe_image(t_imgdata *data)
 {
-	int x;
-	int y;
-	
+	int	x;
+	int	y;
+
 	x = 0;
 	while (x <= WIDTH)
 	{
@@ -37,6 +37,7 @@ int	frame(t_fulldata *fulldata)
 	wipe_image(fulldata->img);
 	draw_horiz_bonus(fulldata);
 	draw_vert_bonus(fulldata);
-	mlx_put_image_to_window(fulldata->mlx, fulldata->mlxwin, fulldata->img->image, 0, 0);
+	mlx_put_image_to_window(fulldata->mlx, fulldata->mlxwin, \
+						fulldata->img->image, 0, 0);
 	return (0);
 }
